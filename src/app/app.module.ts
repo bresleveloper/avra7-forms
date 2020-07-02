6143a3096dc230c62eb3e8ea7b52dedc8975a837
student <stu@ravtech.local>
2020-07-02 13:03:22 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { F1Component } from './forms/f1/f1.component';
+import { F2Component } from './forms/f2/f2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    F1Component,
+    F2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forms/f1/f1.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-f1',
+  templateUrl: './f1.component.html',
+  styleUrls: ['./f1.component.css']
+})
+export class F1Component implements OnInit {
+
+  constructor() { }
+
+  name = new FormControl('', Validators.required);
+  
+  ngOnInit(): void {
+    this.name.valueChanges.subscribe(value => {
+      //console.log(this.name);
+    })
+  }
+
+  logthenamer(){
+    //console.log(this.name);
+
+    console.log({
+      errors:this.name.errors,
+      dirty:this.name.dirty,
+      pristine:this.name.pristine,
+      status:this.name.status,
+      value:this.name.value,
+      valid:this.name.valid,
+      invalid:this.name.invalid,
+      touched:this.name.touched,
+      untouched:this.name.untouched,
+    });
+
+  }
+}
